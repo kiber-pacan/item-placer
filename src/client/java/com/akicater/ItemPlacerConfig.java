0267422f93c1ca7ac8b77dc3845c6d0ae9b6e7e8
kiber-pacan <markpustog@gmail.com>
2024-08-09 11:10:43 +0500

    Original size

--- src/client/java/com/akicater/ItemPlacerConfig.java
+++ src/client/java/com/akicater/ItemPlacerConfig.java
@@ -8,7 +8,7 @@ import static com.akicater.ItemPlacer.MODID;
 
 @Config(name = MODID)
 public class ItemPlacerConfig implements ConfigData {
-    public float absoluteSize = 1.0f;
+    public float absoluteSize = 0.75f;
     public float tempItemSize = 1.0f;
     public float tempBlockSize = 1.0f;
     public boolean oldRendering = false;
