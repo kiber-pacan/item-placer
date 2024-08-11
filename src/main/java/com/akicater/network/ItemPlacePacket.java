616ae54184e2d1c9682aeaf3bdf9c5a606e68ed7
kiber-pacan <markpustog@gmail.com>
2024-08-11 22:32:30 +0500

    Waterlogging

--- src/main/java/com/akicater/network/ItemPlacePacket.java
+++ src/main/java/com/akicater/network/ItemPlacePacket.java
@@ -1,5 +1,6 @@
 package com.akicater.network;
 
+import com.akicater.ItemPlacer;
 import com.akicater.blocks.layingItemBlockEntity;
 import net.fabricmc.fabric.api.networking.v1.PacketSender;
 import net.minecraft.block.BlockState;
@@ -10,7 +11,10 @@ import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.event.GameEvent;
@@ -23,24 +27,28 @@ public class ItemPlacePacket {
         ItemStack stack = player.getMainHandStack();
         ServerWorld world = player.getServerWorld();
         BlockPos pos = buf.readBlockPos();
-        if (world.getBlockState(pos).getBlock() == Blocks.AIR) {
+        BlockHitResult hitResult = buf.readBlockHitResult();
+        if (world.getBlockState(pos).getBlock() == Blocks.AIR || world.getBlockState(pos).getBlock() == Blocks.WATER) {
             player.setStackInHand(Hand.MAIN_HAND, ItemStack.EMPTY);
-            Direction dir = buf.readBlockHitResult().getSide().getOpposite();
-            BlockState state = LAYING_ITEM.getDefaultState();
+            Direction dir = hitResult.getSide().getOpposite();
+            BlockState state = ItemPlacer.LAYING_ITEM.getDefaultState();
+            if (world.getBlockState(pos).getBlock() == Blocks.WATER) {
+                state = state.with(Properties.WATERLOGGED, true);
+            }
             world.setBlockState(pos, state);
             state.initShapeCache();
             layingItemBlockEntity blockEntity = (layingItemBlockEntity)world.getChunk(pos).getBlockEntity(pos);
             if (blockEntity != null) {
-                int i = dirToInt(dir);
+                int i = ItemPlacer.dirToInt(dir);
                 blockEntity.inventory.set(i, stack);
                 world.emitGameEvent(player, GameEvent.BLOCK_CHANGE, pos);
                 blockEntity.markDirty();
             }
-        } else if (world.getBlockState(pos).getBlock() == LAYING_ITEM) {
-            Direction dir = buf.readBlockHitResult().getSide().getOpposite();
+        } else if (world.getBlockState(pos).getBlock() == ItemPlacer.LAYING_ITEM) {
+            Direction dir = hitResult.getSide().getOpposite();
             layingItemBlockEntity blockEntity = (layingItemBlockEntity)world.getChunk(pos).getBlockEntity(pos);
             if (blockEntity != null) {
-                int i = dirToInt(dir);
+                int i = ItemPlacer.dirToInt(dir);
                 if(blockEntity.inventory.get(i).isEmpty()) {
                     player.setStackInHand(Hand.MAIN_HAND, ItemStack.EMPTY);
                     blockEntity.inventory.set(i, stack);
